3c7628eb87e4c45b520a6d7289ac4f9d639b5524
Star-mi <star-mi-7@mail.ru>
2023-09-01 22:42:38 +0300

    add User with UserDetails + Repo

---
+++ src/main/java/com/spring_security/jwt/dao/UserRepository.java
@@ -0,0 +1,12 @@
+package com.spring_security.jwt.dao;
+
+import com.spring_security.jwt.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/spring_security/jwt/user/Role.java
@@ -0,0 +1,6 @@
+package com.spring_security.jwt.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
